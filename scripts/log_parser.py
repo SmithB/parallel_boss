aa56be2d01910087d546a9d4b94011768e2f5c53
Ben Smith <bsmith@apl.washington.edu>
2023-04-10 22:24:56 +0000

    scripts/log_parser.py: utility to print a synopsis of a collection of log files

---
+++ scripts/log_parser.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+import glob
+import re
+import json
+import os
+import pandas as pd
+#import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def get_logs(log_dir):
+    tile_re = re.compile('working on .*/E(.*)_N(.*).h5')
+    trace_line_re = re.compile('(File "(.*)", line (\d+), in (\S+))')
+    error_re = re.compile(r'^((\S+Error): (.*))')
+
+    file_exception = {}
+    file_trace = {}
+    xy_exception = {}
+    xy_trace = {}
+
+
+    for log_file in glob.glob(os.path.join(log_dir,'*.log')):
+        trace_line_info  = None
+        this_xy = None
+        with open(log_file,'r') as fh:
+            for line in fh:
+                m = tile_re.search(line)
+                if m is not None:
+                    this_xy=tuple([*map(int, m.groups())])
+                m = trace_line_re.search(line)
+                if m is not None:
+                    trace_line_info = {'str':m.groups()[0], 'file':m.groups()[1], 'line':m.groups()[2], 'function':m.groups()[3]}
+                m = error_re.search(line)
+                if m is not None:
+                    trace_line_info['exception']=m.groups()[0]
+                    break
+        if trace_line_info is None:
+            continue
+        if trace_line_info['exception'] not in file_exception:
+            file_exception[trace_line_info['exception']] = []
+
+        if trace_line_info['str'] not in file_trace:
+            file_trace[trace_line_info['str']]=[]
+            xy_trace[trace_line_info['str']]=[]
+        file_exception[trace_line_info['exception']] += [log_file]
+        file_trace[trace_line_info['str']] += [log_file]
+        if this_xy is not None:
+            if trace_line_info['exception'] not in xy_exception:
+                xy_exception[trace_line_info['exception']] = []
+            if trace_line_info['str'] not in xy_trace:
+                xy_trace[trace_line_info['str']]=[]
+            xy_exception[trace_line_info['exception']] += [this_xy]
+            xy_trace[trace_line_info['str']] += [this_xy]
+    out={}
+    out['counts']={}
+    out['counts']['by_exception']={key:len(file_exception[key]) for key in file_exception}
+    out['counts']['by_trace']={key:len(file_trace[key]) for key in file_trace}
+    out['by_exception']=file_exception
+    out['by_trace']=xy_trace
+    if len(xy_exception) > 0:
+        out['xy_by_exception']=xy_exception
+        out['xy_by_trace']=xy_trace
+    return out
+
+def main():
+            
+    par_run_dir=sys.argv[1]
+    log_dir=os.path.join(par_run_dir,'logs')
+
+    print('Unique exceptions and counts:')
+    for exc, files in out['by_exception'].items():
+        print(f'\t{exc} : {len(files)}')
+    print('\nUnique trace lines and counts:')
+    for tr, files in out['by_trace'].items():
+        print(f'\t{tr} : {len(files)}')
+
+    out_file=os.path.join(log_dir,'..','log_summary.json')
+    with open(out_file,'w') as fh:
+        json.dump(out, fh, indent=4)
+
+if __name__=='__main__':
+    main()
+
