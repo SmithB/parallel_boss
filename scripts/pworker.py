8fdf528048e53a5cd7c60520ace53638d9109dfd
smithb <smithcommaben@gmail.com>
2024-02-28 15:51:50 -0500

    scripts/pworker.py: added logging options

--- scripts/pworker.py
+++ scripts/pworker.py
@@ -3,7 +3,7 @@
 import sys, os, time, re, subprocess, datetime, stat
 
 class pworker(object):
-    def __init__(self):
+    def __init__(self, log=False):
 
         self.verbose=True
         self.retired=False
@@ -24,6 +24,13 @@ class pworker(object):
         self.to_boss_file=self.comms_to_boss_dir+'/request.txt'
         self.scratch=self.worker_dir+"scratch_from_worker.txt"
 
+        self.log_handle=None
+        if log:
+            log_dir="par_run/worker_logs"
+            if not os.path.isdir(log_dir):
+                os.mkdir(log_dir)
+            self.log_handle=open(os.path.join(log_dir, "worker_%s.%s/"%(self.hostname, self.PID)))
+            self.log('starting')
         if not os.path.isdir(self.worker_dir):
             os.mkdir(self.worker_dir)
         if not os.path.isdir(self.comms_to_worker_dir):
@@ -36,6 +43,10 @@ class pworker(object):
         if not os.path.isdir('par_run/logs'):
             os.mkdir('par_run/logs');
 
+    def log(self, message):
+        if self.log_handle is not None:
+            self.log_handle.write(str(datetime.datetime.now())+':'+message+'\n')
+
     def get_new_job(self):
         response_re=re.compile('response\[(\d+)\]\s+(.*);')
 
@@ -44,7 +55,7 @@ class pworker(object):
         with open(self.scratch,'w') as fid:
             fid.write("request new job %d;\n" % self.comms_count)
         os.rename(self.scratch, self.to_boss_file)
-
+        self.log(f'sent request {self.comms_count}')
         # wait for a response from the boss
         while not os.path.isfile(self.from_boss_file) and not self.retired:
             self.consider_retirement()
@@ -56,12 +67,16 @@ class pworker(object):
         os.remove(self.from_boss_file)
         if self.verbose:
             print("parallel_boss sent line [%s]" % the_line)
+        self.log(f"received response from boss")
+
         response_match=response_re.search(the_line)
         if response_match is None:
+            self.log("response did not match:"+the_line.rstrip())
             return None
         proc_count=response_match.group(1)
         task_file=response_match.group(2)
         if not os.path.isfile(task_file):
+            self.log("Task file %s Does not exist, ignoring request\n" % task_file)
             print("Task file %s Does not exist, ignoring request\n" % task_file)
             return None
         task_num=re.search('task_(\d+)', task_file).group(1)
@@ -73,7 +88,7 @@ class pworker(object):
 
 
     def run_job(self, proc_count, task_file, task_num):
-
+        self.log("starting task_file:"+task_file)
         # move the task file to the running file
         running_file="par_run/running/task_%s_%s_%s.%s"%(task_num, self.hostname, self.PID, proc_count)
         os.rename(task_file, running_file)
@@ -115,7 +130,7 @@ class pworker(object):
         os.rename(running_file, done_file);
         if self.verbose:
             print("------- Finished: %s" % str(datetime.datetime.now()))
-
+            self.log("finished "+task_file)
     def run_loop(self):
         while os.path.isdir(self.comms_to_worker_dir) or self.comms_count==0:
             job_info=self.get_new_job()
@@ -123,13 +138,15 @@ class pworker(object):
             if job_info is not None:
                 self.run_job(*job_info)
             self.consider_retirement()
-            print("outside: retired is:"+str(self.retired))
             if self.retired:
                 break
 
 def __main__():
-
-    this_worker = pworker()
+    if '--log' in sys.argv:
+        log=True
+    else:
+        log=False
+    this_worker = pworker(log=log)
     this_worker.run_loop()
 
 if __name__=='__main__':
